d3cd31bd14c4118edf1ed3a7b362540d761a8c47
Eric Barreto <ericbarreto521@gmail.com>
2024-05-15 22:56:59 -0300

    feat: Criação seção de nossos produtos

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { banner } from "./assets";
 
 export function App() {
   return (
-    <div className="px-40">
+    <div className="text-center">
       <NavBar />
-      <div className="flex justify-center">
-        <img src={banner} className="rounded-3xl"></img>
+
+      <div className="flex justify-center mx-auto max-w-[1400px]">
+        <img src={banner} className="rounded-3xl w-auto"></img>
+      </div>
+
+      <div className="px-40 flex flex-col items-start mt-10">
+        <h2 className="text-3xl font-bold">Nossos produtos</h2>
       </div>
     </div>
   );
--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -3,14 +3,14 @@ import { ShoppingCart } from "lucide-react";
 
 export default function NavBar() {
     return (
-        <div className="flex justify-between items-center p-6">
+        <div className="px-40 flex justify-between items-center p-6">
             <img className="w-40 cursor-pointer" src={logo}></img>
 
             <div className="flex gap-8">
-            <a href="#" className="font-bold ">Home</a>
-            <a href="#" className="font-bold ">Oferta</a>
-            <a href="#" className="font-bold ">Nossos produtos</a>
-            <a href="#" className="font-bold ">Sobre nós</a>
+            <a href="#" className="font-bold text-lg">Home</a>
+            <a href="#" className="font-bold text-lg">Oferta</a>
+            <a href="#" className="font-bold text-lg">Nossos produtos</a>
+            <a href="#" className="font-bold text-lg">Sobre nós</a>
             <a href="#">
                 <ShoppingCart />
             </a>
